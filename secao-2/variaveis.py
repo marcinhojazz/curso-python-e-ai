fad7e442c2e8ff02189e2a68608cb310869d34ea
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-04-26 15:49:49 -0300

    Aula 3. Variaveis

---
+++ secao-2/variaveis.py
@@ -0,0 +1,34 @@
+# quantidade = 10
+
+# estoque = 50
+
+# print(f'a quantidade é = {quantidade}')
+# print('a quantidade de itens = {}'.format(quantidade))
+# print('a quantidade de itens = {} e estoque = {}'.format(quantidade, estoque))
+# print('a quantidade de itens = {valor1} e estoque = {valor2}'.format(valor1=quantidade, valor2h=estoque))
+
+'''
+
+inteiro
+
+float
+
+bool
+
+string
+
+'''
+
+# função type
+
+# tipo = 10 # inteiro
+# tipo = 10.20 # float
+# tipo = True # Bool
+# tipo = 'stringe' # string
+# print(type(tipo))
+
+# print(type('10')) #str
+# print(type(10)) #int
+# print(type(10.20)) #float
+# print(type(True)) #bool
+print(type('10')) #string
