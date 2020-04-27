196cacdf899f0e1720da9e81aa0fd35e4ee32a85
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-04-27 12:22:22 -0300

    Aula 4. Manipulação de strings

---
+++ secao-2/manipulacao-string.py
@@ -0,0 +1,46 @@
+'''
+upper()
+lower()
+len()
+replace('str', 'str')
+count('str')
+title()
+'''
+
+# ___________uppercase e lowercase
+
+# str = 'Marcio Machado Pontes'
+
+# str = str.upper()
+# str = str.lower()
+# print(str[1])
+
+# ___________len
+
+# str = 'marcio'
+
+# print(len(str))
+
+#___________replace
+
+# str = 'marcio machado'
+# str = str.replace('machado', 'pontes')
+# print(str)
+
+#___________count
+
+# str = 'ccc bbb eee cbe'
+# print(str.count('c'))
+
+#___________find
+
+# ['a', 'b', 'c', 'd']
+# str = 'marcio'
+# print(str.find('a'))
+
+
+#___________title
+
+str = 'marcio machado pontes'
+str = str.title()
+print(str)
