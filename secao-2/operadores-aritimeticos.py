0ed64a3479aa8dab48416da868c7a8a6eb9193d7
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-04-27 14:23:28 -0300

    Seção 2 - Curso Python + AI | Aula 6. Operadores aritiméticos

---
+++ secao-2/operadores-aritimeticos.py
@@ -0,0 +1,49 @@
+# SOMA
+
+# n1 = 2
+# n2 = 2
+# soma = 2+2
+
+# print(soma)
+# print(n1 + n2)
+
+# SUBTRAÇÃO
+
+# n1 = 2
+# n2 = 2
+# subtracao = 2-2
+# print(subtracao)
+# print(n1 - n2)
+
+# MULTIPLICAÇÃO
+
+# n1 = 2
+# n2 = 2
+# multplicacao = 2*2
+# print(multplicacao)
+# print(n1 * n2)
+
+# DIVISÃO
+
+# n1 = 4
+# n2 = 2
+# divisao = n1/n2
+# print(divisao)
+# print(n1 / n2) #float
+# print(n1 // n2) #inteiro
+
+# EXPONENIAÇÃO
+
+# n1 = 2
+# n2 = 2
+# expo = n1**n2
+# print(expo)
+# print(n1 ** n2)
+
+# RESTO
+
+n1 = 5
+n2 = 2
+resto = n1%n2
+print(resto)
+print(n1 % n2)
