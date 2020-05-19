3e8b4a43b6e5fc9ba199580f4c9d9e72c83eb259
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-19 15:41:54 -0300

    SEÇÃO 2 | 12. Laços de repetição: For

---
+++ secao-2/lacos_repeticao_for.py
@@ -0,0 +1,19 @@
+# x = [[1, 2, 3, 4, 5], [10, 11, 12, 13, 14, 15]]
+# x = ['caio', 'jorge', 'matheus']
+
+# for i in x:
+#     print('oi')
+#     print(i)
+
+# Crescente
+# for i in range(1, 101):
+#     print(i)
+
+# descrescente
+# for i in range(100, 0, -2):
+# # for i in range(100, 0, -5):
+#     print(i)
+
+#de 0 até 1.000 mostra de 2 em 2 (ou somente números pares)
+for i in range(0, 102, 2):
+    print(i)
