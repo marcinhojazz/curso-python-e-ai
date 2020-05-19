265b1c3eb4c630bd84bef91bf1854ef8ba9097e5
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-19 15:30:11 -0300

    Aula 11 | Laços de repetição: While, parte 2

---
+++ secao-2/lacos-de-repeticao2.py
@@ -0,0 +1,11 @@
+x = -1
+while x < 10:
+    
+    x += 1  #esse código é o mesmo que o código acima
+    if x == 5:
+        continue
+    
+    print(x)
+    # x = x + 1 #esse código é o mesmo que o código abaixo
+
+print('acabou o laço')
