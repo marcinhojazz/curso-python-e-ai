ddeeaaf964ce94984d37ef60b1e285a07b352b5c
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-19 17:08:21 -0300

    SEÇÃO 2 | 16. Estrutura de dados: listas parte 2

---
+++ secao-2/estrutura_dados_list2.py
@@ -0,0 +1,31 @@
+# x = []
+
+# for i in range(1, 1001):
+#     # print(i)
+#     x.append(i)
+
+# print(x)
+
+
+#outra forma de listar
+# x = list(range(1, 1001))
+# print(x)
+
+
+'''
+remove(6)
+'''
+# x = [1, 2, 3, 4, 5]
+# print(x)
+# y = int(input('digite um número para apagar da lista: '))
+# if y in x:
+#     x.remove(y)
+#     print('o valor foi removido com sucesso!!!')
+# print(x)
+
+
+#listas de lista
+x = [['marcio', 18], ['jorge', 25], ['matheus', 45]]
+print(x[0], x[1][0])
+print(x[0][0])
+print(x[0][1])
