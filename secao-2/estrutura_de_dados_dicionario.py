a989e710f7569edade3d80d8775af3d745abceba
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-20 21:27:35 -0300

    SEÇÃO 2 | Aula 17. Estrutura de dados - Dicionário

---
+++ secao-2/estrutura_de_dados_dicionario.py
@@ -0,0 +1,36 @@
+# x = {'nome': 'caio', 'idade': 18, 'telefone': '12345678'}
+
+# print(x)
+# print(x['nome'])
+# print(x['idade'])
+# print(x['telefone'])
+# print()
+
+# print(len(x))
+
+# x.pop('idade')
+
+# print(x)
+
+
+cadastroPessoas = [
+    {
+        'nome': 'marcio',
+        'idade': 28,
+        'telefone':
+        '12345678',
+        'cpf':
+        '1234567890'
+    },
+    {
+        'nome': 'igor',
+        'idade': 32,
+        'telefone':
+        '87654321',
+        'cpf':
+        '0987654321'
+    }
+]
+
+# print(cadastroPessoas)
+print(cadastroPessoas[1]['idade'])
