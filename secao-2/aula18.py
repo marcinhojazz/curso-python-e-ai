49376ba38348c6f01bc9ad230dd16e1b793bc817
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-21 12:40:11 -0300

    SEÇÃO 2 | AULA 18. Modulos

---
+++ secao-2/aula18.py
@@ -0,0 +1,21 @@
+# import modulos
+# import modulos as m
+# from modulos import printa
+# from modulos import valorPadrao
+
+
+# modulos.printa()
+
+# m.printa()
+
+# printa()
+
+# print(valorPadrao)
+
+# v1 = valorPadrao
+
+# print(v1)
+
+from modulos import teste
+
+teste(10)
---
+++ secao-2/modulos.py
@@ -0,0 +1,14 @@
+# def printa():
+#     print('Olá')
+
+# # printa()
+
+# def soma(a, b):
+#     print(a+b)
+
+# valorPadrao = 10
+
+class teste():
+    def __init__(self, a):
+        self.a = a
+        print(self.a)
