c876a2bd5babf325c7e4d6083710752b1717fd07
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-30 20:39:15 -0300

    SEÇÃO 5 | 49. Criando campos para entrada de

--- SECAO3/projeto1/interface.py
+++ SECAO3/projeto1/interface.py
@@ -5,13 +5,25 @@ janela.title('Curso de Python')
 janela.geometry('300x300')
 janela.resizable(False, False)
 
-Label(
-    janela, 
-    text='Texto dentro da janela', 
-    bg='black', 
-    fg='white',
-    pady='30',
-    padx=30
-    ).grid(row=0, column=0)
+
+# 49. Criando campos para entrada de dados - Entry
+# Entry(
+#     janela,
+#     bg='black',
+#     fg='orange',
+#     show='*'
+#      ).grid(row=0, column=0)
+
+# 48. Criando campos para textos - Label
+# Label(
+#     janela, 
+#     text='Texto dentro da janela', 
+#     bg='black', 
+#     fg='white',
+#     pady='30',
+#     padx=30
+#     ).grid(row=0, column=0)
+
+
 
 janela.mainloop()
