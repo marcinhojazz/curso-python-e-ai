504e102f60f520f4c29c34098adb32746ea4b7a5
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-06-01 16:31:01 -0300

    SEÇÃO 5 | 53. Criando um Frame

---
+++ SECAO5/treeview.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import ttk
+
+janela = Tk()
+janela.title('TreeView')
+
+tree = ttk.Treeview(janela, selectmode="browse", column=("column1", "column2", "column3", "column4"), show='headings')
+
+tree.column("column1", width=200, minwidth=50, stretch=NO)
+tree.heading("#1", text='Nome')
+
+tree.column("column2", width=100, minwidth=50, stretch=NO)
+tree.heading("#2", text='Idade')
+
+tree.column("column3", width=300, minwidth=50, stretch=NO)
+tree.heading("#3", text='Endereco')
+
+tree.column("column4", width=50, minwidth=50, stretch=NO)
+tree.heading("#4", text='ID')
+
+
+elementos = ["joaquim", "13", "13, esquina com 16", 1]
+for i in range(0, 4):
+    tree.insert("", END, values=elementos, tag='1')
+    
+
+tree.grid(row=0, column=0)
+
+
+
+janela.mainloop()
