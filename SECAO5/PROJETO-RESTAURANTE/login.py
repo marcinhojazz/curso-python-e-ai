1042e87d1fbac9a6c347a45cbf688239ddfedce1
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-06-02 13:48:02 -0300

    SEÇÃO 5 - PROJETO RESTAURANTE | 56. Cadastre-se Back End

--- SECAO5/PROJETO-RESTAURANTE/login.py
+++ SECAO5/PROJETO-RESTAURANTE/login.py
@@ -25,7 +25,7 @@ class JanelaLogin():
                 charset='utf8mb4',
                 cursorclass=pymysql.cursors.DictCursor
 
-)
+            )
         except:
             print('erro ao conectar no banco de dados')
 
@@ -57,8 +57,51 @@ class JanelaLogin():
             self.root.destroy()
             if usuarioMaster:
                 AdminJanela()
+    
+    def CadastroBackEnd(self):
+        codigoPadrao = '123@h'
+
+        if self.codigoSeguranca.get() == codigoPadrao:
+            if len(self.login.get()) <= 20:
+                if len(self.senha.get()) <= 50:
+                    nome = self.login.get()
+
+                    senha = self.senha.get()
+
+                    try:
+                        conexao = pymysql.connect(
+
+                            host='localhost',
+                            user='root',
+                            password='',
+                            db='erp',
+                            charset='utf8mb4',
+                            cursorclass='pymysql.cursors.DictCursor'
+                        )
+                    except:
+                        print('erro ao conectar ao banco de dados')
+                    
+                    try:
+                        with conexao.cursor() as cursor:
+                            cursor.execute('insert into cadastros (nome, senha, nivel) values (%s, %s, %s)', (nome, senha, 1))
+                            conexao.commit()
+                        messagebox.showinfo('cadastro', 'Usuario cadastrado com sucesso!')
+                    except:
+                        print('erro ao inserir dados')
+
+                else:
+                    messagebox.showinfo('ERRO', 'Por favor, insira uma senha com 50 ou menos caracteres')
+            else:
+                messagebox.showinfo('ERRO', 'Por favor, insira um nome com 20 caracteres')
+        else:
+            messagebox.showinfo('ERRO', 'Erro no código de segurança')
 
 
+    def Cadastro(self):
+        Label(self.root, text='Chave de segurança').grid(row=3, column=0, pady=5, padx=5)
+        self.codigoSeguranca = Entry(self.root, show='*')
+        self.codigoSeguranca.grid(row=3, column=1, pady=5, padx=10)
+        Button(self.root, text='Confirmar cadastros', width=15, bg='blue3', command=self.CadastroBackEnd).grid(row=4, column=0, columnspan=3, pady=5, padx=10)
 
 
     def __init__(self):
@@ -76,11 +119,11 @@ class JanelaLogin():
         self.senha = Entry(self.root, show='*')
         self.senha.grid(row=2, column=1, padx=5, pady=5)
 
-        Button(self.root, text='login', bg='green3', width=10, command=self.VerificaLogin).grid(row=3, column=0, padx=5, pady=5)
+        Button(self.root, text='login', bg='green3', width=10, command=self.VerificaLogin).grid(row=5, column=0, padx=5, pady=5)
 
-        Button(self.root, text='cadastrar', bg='orange3', width=10).grid(row=3, column=1, padx=5, pady=5)
+        Button(self.root, text='cadastrar', bg='orange3', width=10, command=self.Cadastro).grid(row=5, column=1, padx=5, pady=5)
 
-        Button(self.root, text='Visualizar cadastros', bg='white').grid(row=4, column=0, columnspan=2,  padx=5, pady=5)
+        Button(self.root, text='Visualizar cadastros', bg='white').grid(row=6, column=0, columnspan=2,  padx=5, pady=5)
 
         self.root.mainloop()
 
---
+++ SECAO5/PROJETO-RESTAURANTE/teste-sorted.py
@@ -0,0 +1,5 @@
+import random
+sorteio = random.randint(0, 200)
+# print(sorteio)
+for sorteio in x:
+    print(x)
